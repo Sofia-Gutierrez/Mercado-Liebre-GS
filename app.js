9a4b97dae0cb0cabad048a48fc1b5a3190685066
Sofia_Gutierrez <sofiagutierrez1845@gmail.com>
2022-12-29 12:53:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log("servidor corriendo en el puerto " + port)
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./index.html"));
+});
+
+app.get("/form", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/form.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
